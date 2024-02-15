9f1b42a64160c0f2a81ff2dd22b295600951d817
aliciaosv <158157360+aliciaosv@users.noreply.github.com>
2024-02-15 21:56:04 +0100

    Mallat upp komponenter. Kommenterat ut överflödig CSS. Touchat en routes.js. To be continued tomorrow!

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<script setup>
+</script>
+<template>
+  <h4>Här ska footern vara. Följ wireframen för innehållet</h4>
+</template>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+  <h3>Här är den första sidan man kommer in på! Följ wireframen för innehållet
+
+  </h3>
+</template>
