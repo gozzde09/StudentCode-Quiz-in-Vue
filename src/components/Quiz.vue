1470a9931091540cf05aba29c482d4ffb1f98dd7
Jonas Brunhage <jonas.brunhage@gmail.com>
2024-02-16 09:19:24 +0100

    start quiz design

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,1 +1,20 @@
-<template> <h1> TEST QUIZ SIDA</h1></template>
+<template>
+  <div>
+    <h1> JavaScript</h1>
+    <button>X</button>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+button {
+  border-radius: 100%;
+  padding: 5px;
+  text-align: center;
+}
+</style>
