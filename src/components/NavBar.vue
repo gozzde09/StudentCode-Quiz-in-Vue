dfc33feeb45ec28265b9afd9eea11daeb08945d8
aliciaosv <158157360+aliciaosv@users.noreply.github.com>
2024-02-22 11:52:22 +0100

    ny nav, startsida för quiz

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,58 +1,54 @@
 <script setup>
-import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
-const activePage = ref(route.path)
-
-function thisActivePage(path) {
-  activePage.value = path
-}
 </script>
 
 <template>
-  <div class="d-flex justify-content-center">
-    <b-nav pills>
-      <b-nav-item :class="{ active: activePage === '/' }" @click="thisActivePage('/')">
-        <RouterLink to="/" class="nav-text">Home</RouterLink>
-      </b-nav-item>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-xxl">
 
-      <!-- Dropdownlänkarna  -->
-      <b-nav-item-dropdown text="Quiz" toggle-class="nav-link-custom" right >
-        <b-dropdown-item :class="{ active: activePage === '/HtmlPage' }" @click="thisActivePage('/HtmlPage')">
-          <RouterLink class="dropdown-text" to="/HtmlPage">HTML</RouterLink>
-        </b-dropdown-item>
-        <b-dropdown-item  :class="{ active: activePage === '/JavascriptPage' }" @click="thisActivePage('/JavascriptPage')"
-          >
-          <RouterLink class="dropdown-text" to="/JavascriptPage">JavaScript</RouterLink>
-        </b-dropdown-item>
-        <b-dropdown-item :class="{ active: activePage === '/PhpPage' }" @click="thisActivePage('/PhpPage')">
-          <RouterLink class="dropdown-text" to="/PhpPage">PHP</RouterLink>
-        </b-dropdown-item>
-      </b-nav-item-dropdown>
+      <!-- toggle-knapp för mobile-view -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav"
+        aria-controls="main-nav" aria-expanded="false" aria-label="Toggle Navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-      <b-nav-item :class="{ active: activePage === '/MyResults' }" @click="thisActivePage('/MyResults')">
-        <RouterLink to="/MyResults" class="nav-text">My Results</RouterLink>
-      </b-nav-item>
-    </b-nav>
-  </div>
+      <!-- navbarlänkar -->
+      <div class="collapse navbar-collapse justify-content-lg-center align-center" id="main-nav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link" :class="{ active: $route.path === '/' }">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/QuizPage" class="nav-link"
+              :class="{ active: $route.path.startsWith('/QuizPage') }">Quiz</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/MyResults" class="nav-link" :class="{ active: $route.path === '/MyResults' }">My
+              Results</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/About" class="nav-link" :class="{ active: $route.path === '/About' }">About
+              Us</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
-.nav-text {
-  color: black;
-  text-decoration: none;
+.nav-link {
+  padding: 10px;
+  margin: 5px;
+  height: 40px;
 }
 
 .active {
   background-color: #EDE8E3;
   color: black;
-  padding: 3px;
   border-radius: 2px;
   transition: background-color 0.3s ease;
-}
-.dropdown-text{
-  color: black ;
-  text-decoration: none ;
 }
 </style>
