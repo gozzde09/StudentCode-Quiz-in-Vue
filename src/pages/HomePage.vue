85bf227b7c564d9dccfa1f0160030b7ea0ae88e8
Gzd Akgn <gozzde0990@gmail.com>
2024-02-26 22:17:55 +0100

    footer, design, progressbar, clean code

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,33 +3,33 @@
 
 </script>
 <template>
-  <div class="d-flex flex-column mx-auto">
-    <h1 class="mx-auto">Welcome!</h1>
-    <div class="row justify-content-center">
+  <div class="d-flex flex-column mx-auto justify-content-evenly">
+    <h1 class="mx-auto my-3">Welcome!</h1>
+    <div class="mx-auto row justify-content-center">
       <div class="col-md-10">
         <div class="jumbotron">
-          <h3>At StudentCode you can test your programming and coding skills.</h3>
+          <h3 class="mx-auto">At StudentCode you can test your programming and coding skills.</h3>
           <p class="lead">Discover our range of quizzes to deepen your knowledge
             your skills and have fun at the same time!</p>
         </div>
-        <div class="d-flex">
-          <button class="mx-auto">
-             <router-link to="/StartQuiz" class="btn link mx-auto">Start quiz</router-link> </button>
-        </div>
       </div>
     </div>
-
+    <div class="d-flex">
+      <button class="mx-auto">
+        <router-link to="/StartQuiz" class="btn nav-link mx-auto">Start quiz</router-link> </button>
+    </div>
   </div>
 </template>
 <style scoped>
 h1 {
   color: rgb(32, 71, 100);
-  margin: 2rem;
   font-weight: bold;
 }
 
-button, .link {
+button {
   background-color: #204764 !important;
   color: #ffffff !important;
+  padding: 10px;
 }
+
 </style>
