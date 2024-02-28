f537648087555b59dcef16309e17adcbc31dfd29
aliciaosv <158157360+aliciaosv@users.noreply.github.com>
2024-02-28 11:16:21 +0100

    layout quizval klar

--- src/pages/StartQuiz.vue
+++ src/pages/StartQuiz.vue
@@ -5,8 +5,8 @@ const route = useRoute()
 </script>
 
 <template>
-  <div class="container">
-    <header class="row justify-content-center" id="the-jumbo">
+  <div class="container text-center my-5">
+    <header class="row justify-content-center mb-5" id="the-jumbo">
       <div class="col-md-6">
         <div class="jumbotron">
           <h4 class="display-4">Alright, let's get started!</h4>
@@ -16,56 +16,55 @@ const route = useRoute()
       </div>
     </header>
     <!-- Topic -->
-    <div class="container justify-content-center">
+    <div class="container justify-content-center mb-3">
       <h6 class="display-6">Choose a topic</h6>
       <div class="btn-group" role="group" aria-label="Topic">
         <input type="radio" class="btn-check" name="topic" id="html" autocomplete="off">
-        <label class="btn btn-secondary" for="html">Html</label>
+        <label class="btn btn-outline-secondary" for="html">Html</label>
 
         <input type="radio" class="btn-check" name="topic" id="javascript" autocomplete="off">
-        <label class="btn btn-secondary" for="javascript">JavaScript</label>
+        <label class="btn btn-outline-secondary" for="javascript">JavaScript</label>
 
         <input type="radio" class="btn-check" name="topic" id="php" autocomplete="off">
-        <label class="btn btn-secondary" for="php">PHP</label>
+        <label class="btn btn-outline-secondary" for="php">PHP</label>
 
         <input type="radio" class="btn-check" name="topic" id="wordpress" autocomplete="off">
-        <label class="btn btn-secondary" for="wordpress">Wordpress</label>
+        <label class="btn btn-outline-secondary" for="wordpress">Wordpress</label>
       </div>
     </div>
     <!-- Difficulty -->
-    <div class="container justify-content-center">
+    <div class="container justify-content-center mb-3">
       <h6 class="display-6">Choose difficulty</h6>
       <div class="btn-group" role="group" aria-label="Difficulty">
 
         <input type="radio" class="btn-check" name="difficulty" id="easy" autocomplete="off">
-        <label class="btn btn-secondary" for="easy">Easy</label>
+        <label class="btn btn-outline-secondary" for="easy">Easy</label>
 
         <input type="radio" class="btn-check" name="difficulty" id="medium" autocomplete="off">
-        <label class="btn btn-secondary" for="medium">Medium</label>
+        <label class="btn btn-outline-secondary" for="medium">Medium</label>
 
         <input type="radio" class="btn-check" name="difficulty" id="hard" autocomplete="off">
-        <label class="btn btn-secondary" for="hard">Hard</label>
+        <label class="btn btn-outline-secondary" for="hard">Hard</label>
       </div>
     </div>
     <!-- Antal frågor -->
-    <div class="container justify-content-center">
+    <div class="container justify-content-center mb-3">
       <h6 class="display-6">Choose amount of questions</h6>
       <div class="btn-group" role="group" aria-label="Question amount">
         <input type="radio" class="btn-check" name="questionAmount" id="q5" autocomplete="off">
-        <label class="btn btn-secondary" for="q5">5</label>
+        <label class="btn btn-outline-secondary" for="q5">5</label>
 
         <input type="radio" class="btn-check" name="questionAmount" id="q10" autocomplete="off">
-        <label class="btn btn-secondary" for="q10">10</label>
+        <label class="btn btn-outline-secondary" for="q10">10</label>
 
         <input type="radio" class="btn-check" name="questionAmount" id="q15" autocomplete="off">
-        <label class="btn btn-secondary" for="q15">15</label>
+        <label class="btn btn-outline-secondary" for="q15">15</label>
 
         <input type="radio" class="btn-check" name="questionAmount" id="q20" autocomplete="off">
-        <label class="btn btn-secondary" for="q20">20</label>
+        <label class="btn btn-outline-secondary" for="q20">20</label>
       </div>
     </div>
-
-
+    <button type="button" class="btn btn-secondary btn-lg">Start Quiz!</button>
 
   </div>
 </template>
