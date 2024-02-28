ee885996654409f93655833a611211324ffd3475
aliciaosv <158157360+aliciaosv@users.noreply.github.com>
2024-02-28 10:48:57 +0100

    checkboxes istället för dropdowns. fixar adressparametrar och @click asap

--- src/pages/StartQuiz.vue
+++ src/pages/StartQuiz.vue
@@ -15,52 +15,53 @@ const route = useRoute()
         </div>
       </div>
     </header>
-
+    <!-- Topic -->
     <div class="container justify-content-center">
       <h6 class="display-6">Choose a topic</h6>
-      <div class="dropdown">
-        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-          data-bs-toggle="dropdown" aria-expanded="false">
-          Topic
-        </a>
+      <div class="btn-group" role="group" aria-label="Topic">
+        <input type="radio" class="btn-check" name="topic" id="html" autocomplete="off">
+        <label class="btn btn-secondary" for="html">Html</label>
 
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-          <li><a class="dropdown-item" href="#">Html</a></li>
-          <li><a class="dropdown-item" href="#">JavaScript</a></li>
-          <li><a class="dropdown-item" href="#">PHP</a></li>
-          <li><a class="dropdown-item" href="#">Wordpress</a></li>
-        </ul>
+        <input type="radio" class="btn-check" name="topic" id="javascript" autocomplete="off">
+        <label class="btn btn-secondary" for="javascript">JavaScript</label>
+
+        <input type="radio" class="btn-check" name="topic" id="php" autocomplete="off">
+        <label class="btn btn-secondary" for="php">PHP</label>
+
+        <input type="radio" class="btn-check" name="topic" id="wordpress" autocomplete="off">
+        <label class="btn btn-secondary" for="wordpress">Wordpress</label>
       </div>
     </div>
+    <!-- Difficulty -->
     <div class="container justify-content-center">
       <h6 class="display-6">Choose difficulty</h6>
-      <div class="dropdown">
-        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-          data-bs-toggle="dropdown" aria-expanded="false">
-          Difficulty
-        </a>
+      <div class="btn-group" role="group" aria-label="Difficulty">
 
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-          <li><a class="dropdown-item" href="#">Easy</a></li>
-          <li><a class="dropdown-item" href="#">Medium</a></li>
-          <li><a class="dropdown-item" href="#">Hard</a></li>
-        </ul>
+        <input type="radio" class="btn-check" name="difficulty" id="easy" autocomplete="off">
+        <label class="btn btn-secondary" for="easy">Easy</label>
+
+        <input type="radio" class="btn-check" name="difficulty" id="medium" autocomplete="off">
+        <label class="btn btn-secondary" for="medium">Medium</label>
+
+        <input type="radio" class="btn-check" name="difficulty" id="hard" autocomplete="off">
+        <label class="btn btn-secondary" for="hard">Hard</label>
       </div>
     </div>
+    <!-- Antal frågor -->
     <div class="container justify-content-center">
       <h6 class="display-6">Choose amount of questions</h6>
-      <div class="dropdown">
-        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-          data-bs-toggle="dropdown" aria-expanded="false">
-          Question amount
-        </a>
+      <div class="btn-group" role="group" aria-label="Question amount">
+        <input type="radio" class="btn-check" name="questionAmount" id="q5" autocomplete="off">
+        <label class="btn btn-secondary" for="q5">5</label>
 
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-          <li><a class="dropdown-item" href="#">5</a></li>
-          <li><a class="dropdown-item" href="#">10</a></li>
-          <li><a class="dropdown-item" href="#">15</a></li>
-          <li><a class="dropdown-item" href="#">20</a></li>
-        </ul>
+        <input type="radio" class="btn-check" name="questionAmount" id="q10" autocomplete="off">
+        <label class="btn btn-secondary" for="q10">10</label>
+
+        <input type="radio" class="btn-check" name="questionAmount" id="q15" autocomplete="off">
+        <label class="btn btn-secondary" for="q15">15</label>
+
+        <input type="radio" class="btn-check" name="questionAmount" id="q20" autocomplete="off">
+        <label class="btn btn-secondary" for="q20">20</label>
       </div>
     </div>
 
