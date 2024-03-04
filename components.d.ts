655a8d111e6191943637f41b9c71eacebb922328
Gzd Akgn <gozzde0990@gmail.com>
2024-03-04 20:37:02 +0100

    clean up

--- components.d.ts
+++ components.d.ts
@@ -8,11 +8,15 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     BButton: typeof import('bootstrap-vue-next')['BButton']
+    BModal: typeof import('bootstrap-vue-next')['BModal']
     FooterPart: typeof import('./src/components/FooterPart.vue')['default']
     GAMLA: typeof import('./src/components/GAMLA.vue')['default']
     GetQuiz: typeof import('./src/components/GetQuiz.vue')['default']
     NavBar: typeof import('./src/components/NavBar.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+  }
+  export interface ComponentCustomProperties {
+    vBModal: typeof import('bootstrap-vue-next')['vBModal']
   }
 }
