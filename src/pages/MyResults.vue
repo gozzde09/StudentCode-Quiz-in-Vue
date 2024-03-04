06be6798f1b5221b5949b45ce1fb352184a049d8
Emnet Debesay <emnet.debesay@iths.se>
2024-03-04 14:20:55 +0100

    my results

--- src/pages/MyResults.vue
+++ src/pages/MyResults.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="d-flex flex-column mx-auto justify-content-evenly">
-      <h1 class="display-4 mx-auto my-3 rubrik">Your Results!</h1>
-      <div class="mx-auto row justify-content-center">
-        <div class="col-md-10">
-          <div class="jumbotron">
-            <p class="lead"></p>
-            <p class="lead"></p>
-          </div>
+    <h1 class="display-4 mx-auto my-3">Your Results!</h1>
+    <div class="mx-auto row justify-content-center">
+      <div class="col-md-10">
+        <div class="jumbotron">
+          <p class="lead"></p>
+          <p class="lead"></p>
         </div>
       </div>
     </div>
+  </div>
 
   <div class="row justify-content-center" id="result-container">
     <div class="col-md-2">
@@ -18,7 +18,7 @@
           <div class="card">
             <div class="card-body">
               <h5 class="card-title">{{ result.quiz }}</h5>
-              <p class="card-text">Your score: {{ result.score * 100 }}%</p>
+              <p class="card-text">You got: {{ result.score }}</p>
             </div>
           </div>
         </div>
@@ -29,21 +29,16 @@
 
 <script>
 export default {
-  data() {
-    return {
-      results: [
-        { quiz: "HTML", score: 1/5 },
-        { quiz: "JavaScript", score: 5/5 },
-        { quiz: "PHP", score: 4/5 },
-        { quiz: "WordPress", score: 3/5 }
-      ]
-    };
+  props: {
+    results: {
+      type: Array,
+      required: true
+    }
   }
 }
 </script>
 
 <style scoped>
-
 .card {
   padding: 10px;
   margin-bottom: 10px;
@@ -55,11 +50,18 @@ export default {
   margin: 6px;
   padding: 10px;
 }
+
+h1 {
+  color: rgb(21, 52, 76);
+  font-weight: 400;
+}
+
 .card-title {
   font-size: 1.5rem;
-}  
+}
 
 .card-text {
   font-size: 1rem;
 }
 </style>
+
