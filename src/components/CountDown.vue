64c72e937bd5b9ac16b2a5a6aa7610b2d19ad5df
aliciaosv <158157360+aliciaosv@users.noreply.github.com>
2024-03-04 22:46:21 +0100

    Nedräkningen fungerar igen. 'Result in...' ska fixas. Im on it jag lovar <3

---
+++ src/components/CountDown.vue
@@ -0,0 +1,22 @@
+<script setup>
+  import { ref, onMounted } from 'vue'
+
+  const time = 90
+  const timeleft = ref(time)
+  const timeout = ref(false)
+
+  const aliciastimer = () => {
+    const interval = setInterval(() => {
+      timeleft.value--
+
+      if(timeleft.value <= 0) {
+        stoptimer(interval)
+        timeout.value = true
+      }
+    }, 1000)
+  }
+
+  onMounted(() => {
+    aliciastimer()
+  })
+</script>
