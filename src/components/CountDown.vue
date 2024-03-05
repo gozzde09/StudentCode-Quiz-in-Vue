78990277b7204c77c9e07d22a4e59b5bed3f278d
aliciaosv <158157360+aliciaosv@users.noreply.github.com>
2024-03-05 13:47:10 +0100

    Meddelande visas när tiden rinner ut. Ska bli modalruta

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -1,7 +1,7 @@
 <script setup>
   import { ref, onMounted, watch } from 'vue'
 
-  const time = 90
+  const time = 5
   const timeleft = ref(time)
   const timeout = ref(false)
   let intervalId
@@ -10,12 +10,13 @@
   const aliciastimer = () => {
     intervalId = setInterval(() => {
       if(timeleft.value > 0) {
-        stoptimer(intervalId)
         timeleft.value--
       } else {
-        clearInterval(intervalId)
+        // stoptimer(intervalId)
         timeout.value = true
         notime.value = true
+        console.log(notime.value)
+        clearInterval(intervalId)
       }
     }, 1000)
   }
@@ -34,25 +35,10 @@
 </script>
 
 <template>
-  <div >
-    <p>{{ timeleft }}</p>
+  <div>
+    <p> {{ timeleft }} seconds remaining</p>
   </div>
   <div v-if="notime">
-    <div class="modal" tabindex="-1">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Oh nooo!</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <p>You ran out of time. Would you like to go back and try again?</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Go back to quiz</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    Oh no! You ran out of time
   </div>
 </template>
