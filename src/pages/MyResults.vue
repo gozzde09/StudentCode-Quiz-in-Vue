c87b611860fbb5271549a7e39c6f8bc458ede51f
Emnet Debesay <emnet.debesay@iths.se>
2024-03-05 11:19:56 +0100

    myresults

--- src/pages/MyResults.vue
+++ src/pages/MyResults.vue
@@ -1,12 +1,20 @@
 <script setup>
 import DifficultyComp from '../components/DifficultyComp.vue';
 import { ref, onMounted } from 'vue';
-const localStorageData = ref([]);
+
+const localStorageData = ref(null);
+
 // Function to retrieve data from local storage
 const retrieveDataFromLocalStorage = () => {
-  localStorageData.value = Object.keys(localStorage)
-    .filter(key => key.startsWith('quizDataObject_'))
-    .map(key => JSON.parse(localStorage.getItem(key)));
+  const latestQuizKey = 'latestQuizData'; // Nyckel för senast spelade quiz
+  const latestQuizData = JSON.parse(localStorage.getItem(latestQuizKey));
+  
+  // Om det finns data för senast spelade quiz, sätt den som localStorageData
+  if (latestQuizData) {
+    localStorageData.value = [latestQuizData];
+  } else {
+    localStorageData.value = null;
+  }
 }
 
 // Call the function on component mount
@@ -18,35 +26,36 @@ onMounted(() => {
 <template>
   <!-- Display local storage data -->
   <h1 class="display-4 mx-auto my-3 rubrik">Your Results!</h1>
-  <div class="d-flex row mx-auto justify-content-evenly" v-if="localStorageData">
-    <div class="d-flex flex-wrap mx-auto justify-content-evenly card" v-for="data in localStorageData"
-      :key="data.category">
-      <div class="d-flex">
-        <h1 class="display-4 mx-auto rubrik">{{ data.category }}</h1>
-        <DifficultyComp :difficulty="data.difficulty" />
-      </div>
-      <div class="mx-auto row justify-content-center">
-        <div class="col-md-10">
-          <div class="jumbotron">
-            <h3 class="mx-auto">
-              YOU GOT: <strong class="result">{{
-                data.correctAnswers }}/ {{ data.questionAmount }}</strong>
-            </h3>
-            <p class="lead mx-auto">
-              Would you like to make another quiz or go to your result page?
-            </p>
-          </div>
+  <div v-if="localStorageData">
+    <div class="d-flex row mx-auto justify-content-evenly">
+      <div class="d-flex flex-wrap mx-auto justify-content-evenly card" v-for="data in localStorageData"
+        :key="data.category">
+        <div class="d-flex">
+          <h1 class="display-4 mx-auto rubrik">{{ data.category }}</h1>
+          <DifficultyComp :difficulty="data.difficulty" />
         </div>
-        <div class="d-flex flex-wrap justify-content-between">
-          <router-link to="/QuizStart" class="btn blueBtn backBtn mx-auto my-4">
-            Try again
-          </router-link>
+        <div class="mx-auto row justify-content-center">
+          <div class="col-md-10">
+            <div class="jumbotron">
+              <h3 class="mx-auto">
+                YOU GOT: <strong class="result">{{ data.correctAnswers }}/ {{ data.questionAmount }}</strong>
+              </h3>
+              <p class="lead mx-auto">
+                Would you like to make another quiz or go to your result page?
+              </p>
+            </div>
+          </div>
+          <div class="d-flex flex-wrap justify-content-between">
+            <router-link to="/QuizStart" class="btn blueBtn backBtn mx-auto my-4">
+              Try again
+            </router-link>
+          </div>
         </div>
       </div>
     </div>
   </div>
   <div v-else>
-    <h4>DO MORE QUIZ!</h4>
+    <h4>No quiz results found!</h4>
   </div>
 </template>
 
@@ -71,6 +80,7 @@ onMounted(() => {
   font-size: 1rem;
 }
 </style>
+
 
 
 
