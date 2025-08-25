926262bf9362dba673311d1fa0e1f9e2280192a6
Gzd Akgn <gozzde0990@gmail.com>
2025-08-25 11:47:44 +0200

    video - tabel for results

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,16 +4,28 @@
     <div class="mx-auto row justify-content-center">
       <div class="col-md-10">
         <div class="jumbotron">
-          <h3 class="mx-auto">At StudentCode you can test your programming and coding skills.</h3>
-          <p class="lead">Discover our range of quizzes to deepen your knowledge
-            your skills and have fun at the same time!</p>
+          <h3 class="text-center">
+            At StudentCode you can test your programming and coding skills.
+          </h3>
+          <p class="lead text-center">
+            Discover our range of quizzes to deepen your knowledge your skills
+            and have fun at the same time!
+          </p>
         </div>
+      </div>
+      <div class="d-flex">
+        <video class="mx-auto" height="90%" width="60%" controls>
+          <source src="/src/assets/StudentCodeIntro.mp4" type="video/mp4" />
+        </video>
       </div>
     </div>
     <div class="d-flex">
-       <router-link to="/QuizPage" class="btn blueBtn btn-secondary btn-lg mx-auto my-4">
-           Go to quizzes
-        </router-link>
-      </div>
+      <router-link
+        to="/QuizPage"
+        class="btn blueBtn btn-secondary btn-lg mx-auto"
+      >
+        Go to quizzes
+      </router-link>
+    </div>
   </div>
 </template>
--- src/pages/MyResults.vue
+++ src/pages/MyResults.vue
@@ -1,73 +1,101 @@
 <script setup>
-import { useCategoryStore } from '../store.js'
-const categoryStore = useCategoryStore()
-import DifficultyComp from '../components/DifficultyComp.vue';
-import { ref, onMounted } from 'vue';
-const localStorageData = ref([]);
-// Function to retrieve data from local storage
-const retrieveDataFromLocalStorage = () => {
-  localStorageData.value = Object.keys(localStorage)
-    .filter(key => key.startsWith('quizDataObject_'))
-    .map(key => JSON.parse(localStorage.getItem(key)));
-}
+  import { ref, onMounted } from 'vue'
+  import { useCategoryStore } from '../store.js'
+  const categoryStore = useCategoryStore()
+  import DifficultyComp from '../components/DifficultyComp.vue'
 
-// Call the function on component mount
-onMounted(() => {
-  retrieveDataFromLocalStorage();
-});
+  const localStorageData = ref([])
+  // Hämta data från local storage
+  const getDataFromLocalStorage = () => {
+    localStorageData.value = Object.keys(localStorage)
+      .filter((key) => key.startsWith('quizDataObject_'))
+      .map((key) => JSON.parse(localStorage.getItem(key)))
+      .sort((a, b) => a.category.localeCompare(b.category)) //alfabetisk
+  }
 
+  // Anropa  funktionen
+  onMounted(() => {
+    getDataFromLocalStorage()
+  })
 </script>
 
 <template>
-  <!-- Display local storage data -->
-  <h1 class=" mx-auto mt-4 rubrik">Results from previous quizzes</h1>
-  <div class="d-flex row flex-wrap justify-content-evenly my-4" v-if="localStorageData.length > 0">
-    <div class="d-flex flex-wrap card" v-for="data in localStorageData" :key="data.category">
-      <div class="d-flex align-items-center justify-content-around">
-        <h3 class=" rubrik my-2">{{ data.category }}</h3>
-        <DifficultyComp :difficulty="data.difficulty" />
-      </div>
+  <!-- Display local storage data in a table -->
+  <div
+    class="d-flex flex-column flex-wrap justify-content-center my-4 card mx-auto"
+    v-if="localStorageData.length > 0"
+  >
+    <h1 class="mx-auto mt-4 rubrik text-center">
+      Results from previous quizzes
+    </h1>
+    <h4 class="mx-4 mt-2 text-center">
+      Your recent scores in these difficulties and categories. Are you satisfied
+      or would you like to renew it? It's up to you!
+    </h4>
 
-      <div class="mx-auto row justify-content-center">
-        <div class="col-md-10">
-          <div class="jumbotron d-flex flex-column mx-auto">
-            <h6>
-              Your result: <strong class="result">{{
-                data.correctAnswers }}/{{ data.questionAmount }}</strong>
+    <table
+      class="table justify-content-between mx-auto my-4"
+      style="max-width: 70%"
+    >
+      <thead class="justify-content-between mx-auto">
+        <tr>
+          <th>Category</th>
+          <th>Difficulty</th>
+          <th>Result</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="data in localStorageData" :key="data.category">
+          <td>
+            <h4 class="rubrik">{{ data.category }}</h4>
+          </td>
+          <td>
+            <DifficultyComp :difficulty="data.difficulty" />
+          </td>
+          <td>
+            <h6 class="my-2">
+              Your result:
+              <strong class="result"
+                >{{ data.correctAnswers }}/{{ data.questionAmount }}</strong
+              >
             </h6>
-            <p class=" mx-auto">
-              Your recent score in this difficulty and category. Are you satisfied or would you like to renew it? It's
-              up to you!
-            </p>
-            <router-link to="/QuizStart" @click="categoryStore.setCategory(data.category)"
-              class="btn btn-outline-success mx-auto my-2">
+          </td>
+          <td>
+            <router-link
+              to="/QuizStart"
+              @click="categoryStore.setCategory(data.category)"
+              class="btn btn-outline-success"
+            >
               Try again
             </router-link>
-          </div>
-        </div>
-      </div>
-    </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
   <div v-else>
-    <div class="d-flex flex-wrap justify-content-evenly card mx-auto">
-      <div class="d-flex align-items-center">
-        <h2 class=" mx-auto rubrik my-4">Oops!</h2>
+    <div
+      class="d-flex flex-wrap justify-content-evenly card mx-auto my-4"
+      style="max-width: 40%"
+    >
+      <div class="d-flex align-items-center my-4">
+        <h2 class="mx-auto rubrik my-2">Oops!</h2>
       </div>
 
       <div class="mx-auto row justify-content-center">
         <div class="col-md-10">
           <div class="jumbotron">
-            <h5 class="mx-auto">
-              You haven't done any quizzes yet!
-            </h5>
-            <p class="lead mx-auto">
-              Would you like to do a quiz?
-            </p>
+            <h5 class="mx-auto">You haven't done any quizzes yet!</h5>
+            <p class="lead mx-auto">Would you like to do a quiz?</p>
           </div>
         </div>
 
         <div class="d-flex flex-wrap justify-content-between">
-          <router-link to="/QuizStart" class="btn btn-outline-success mx-auto my-4">
+          <router-link
+            to="/QuizStart"
+            class="btn btn-outline-success mx-auto my-4"
+          >
             Start a quiz
           </router-link>
         </div>
@@ -77,11 +105,20 @@ onMounted(() => {
 </template>
 
 <style scoped>
+  .card {
+    padding: 0px;
+    margin-bottom: 2rem;
+  }
 
-.card {
-  padding: 0px;
-  margin-bottom: 1rem;
-  max-width: 300px;
-}
+  @media only screen and (min-width: 530px) {
+    .card {
+      max-width: 80%;
+    }
+  }
 
+  @media only screen and (min-width: 850px) {
+    .card {
+      max-width: 70%;
+    }
+  }
 </style>
